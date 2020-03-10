388e1c7052cf3be8a32082b84a7277b791c5a9e3
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2020-03-10 12:40:21 +0200

    kokeillaas uusiksi

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
 const express = require('express');
 const app = express();
-app.listen(3000,()=>console.log("kuuntelen"));
+//const db = require('./quaries');
+//const bodyParser = require('-/quaries');
+const port = process.env.PORT || 3000;
+app.listen(port,()=>console.log("kuuntelen" + port));
 app.use(express.static("public"));
+
+app.use(express.json({limit:'1mb'}));
+
+const highscore =[
+  {
+    "Name": "Taavi",
+    "Score": "10"
+  },
+  {
+    "Name": "Ismo",
+    "Score": "8"
+  },
+  {
+    "Name": "Arzga",
+    "Score": "6"
+  }
+]
+app.get('/api/highscore', function (request, response){
+  response.send(highscore);
+})
+
+app.post('/api/highscore', function (request,response)
+{
+  response.send(highscore);
+})
